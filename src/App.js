47a891de722cd0b7dcdda3a9a74a917cdfbd14c3
TonyChen0724 <tonytonytony0724@gmail.com>
2018-09-09 10:59:48 +1200

    make index content (1)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 // test my github token
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <Router>
+      <React.Fragment>
+        <Navbar/>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={index}/>
+          </Switch>
+        
+        </div>
+      </React.Fragment>
+      </Router>
     );
   }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-dark bg-dark mb-5">
-      span.navbar-brand.mb-0.h1.mx-auto
-    </nav>);
+      <span className="navbar-brand mb-0 h1 mx-auto">LyricFinder</span>
+
+    </nav>
+  )
 }
 
-export default Navbar;
+export default Navbar

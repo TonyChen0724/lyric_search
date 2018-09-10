30c0c486450c0b4844984b2aa3fcec98c6a6652a
Xinru Chen <tonytonytony0724@gmail.com>
2018-09-10 17:19:25 +1200

    complete index but error exist

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 // test my github token
 import React, { Component } from 'react';
-import './App.css';
+
 
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
-
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Router>
       <React.Fragment>
-        <Navbar/>
+        <Navbar />
         <div className="container">
           <Switch>
             <Route exact path="/" component={Index}/>

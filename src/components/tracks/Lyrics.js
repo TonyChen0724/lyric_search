4af79b8c7552be9768070a617887740b0b0bf07f
Xinru Chen <tonytonytony0724@gmail.com>
2018-09-12 14:35:02 +1200

    build lyrics page

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Spinner from '../layout/Spinner';
+import {Link} from 'react-router-dom';
 
 class Lyrics extends Component {
 
@@ -11,19 +13,61 @@ class Lyrics extends Component {
     componentDidMount() {
         axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=c0633d5aaa2b27ace7481b0dbf3da53b`)
         .then(res=> {
-            console.log(res.data);
-            //this.setState({track_list: res.data.message.body.track_list});
+            this.setState({lyrics: res.data.message.body.lyrics});
+
+
+            return axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=c0633d5aaa2b27ace7481b0dbf3da53b`);
         })
-        
+        .then(res => {
+          this.setState({track: res.data.message.body.track});
+        })
         .catch(err=> console.log(err));
     }
 
   render() {
-    return (
-      <div>
-        <h1>Lyrics</h1>
-      </div>
-    )
+
+    const {track, lyrics} = this.state;
+    if (track === undefined || lyrics === undefined || Object.keys(track).length === 0 || Object.keys(lyrics).length === 0) {
+      return <Spinner />;
+    } else {
+      return (
+        <React.Fragment>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">Go Back</Link>
+          <div className="card">
+            <h5 className="card-header">
+              {track.track_name} by <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+
+            <div className="card-body">
+              <p className="card-text">{lyrics.lyrics_body}</p>
+            
+            </div>
+
+          </div>
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Album ID</strong>: {track.album_id}
+            </li>
+
+            <li className="list-group-item">
+              <strong>Song Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+            </li>
+
+            <li className="list-group-item">
+              <strong>Explicit Words</strong>{track.explicit === 0 ? 'No' : 'Yes'}
+            </li>
+
+            <li className="list-group-item">
+              
+            </li>
+
+
+          </ul>
+
+        </React.Fragment>
+
+      );
+    }
   }
 }
 
